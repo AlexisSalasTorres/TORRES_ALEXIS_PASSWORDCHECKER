09ca0e2229864072837aace55c2d17a7b3465a7a
Admin <Admin@EA611-07>
2025-01-20 19:59:17 +0800

    Add project files.

---
+++ TORRES_ALEXIS_PASSWORDCHECKER/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TORRES_ALEXIS_PASSWORDCHECKER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a password: ");
+            string password = Console.ReadLine();
+
+            bool isStrong = password.Length >= 8 &&
+                            HasUppercase(password) &&
+                            HasLowercase(password) &&
+                            HasDigit(password) &&
+                            HasSpecialCharacter(password);
+            Console.WriteLine(isStrong ? "Password is Strong" : "Password is Weak");
+            Console.ReadLine();
+        }
+        static bool HasUppercase(string input) => input != input.ToLower();
+        static bool HasLowercase(string input) => input != input.ToUpper();
+        static bool HasDigit(string input) => input.Any(char.IsDigit);
+        static bool HasSpecialCharacter(string input) => input.Any(ch => !char.IsLetterOrDigit(ch));
+        
+    }
+   
+
+}
